4a1f064387adad839f357a27eabebe5a1afeba50
yyeonjju <llilyha123@gmail.com>
2024-02-17 18:15:46 +0900

    Add : color asset 추가 및 SwiftGen 사용하여 컬러 스트링은 코드화  https://github.com/yyeonjju/Interview_Questions/issues/15

--- FillTheEgg/UIComponent/ViewController.swift
+++ FillTheEgg/UIComponent/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     let label : UILabel = {
         var label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         label.textColor = .black
-        label.textColor = UIColor(named : "mainYellow")
+//        label.textColor = UIColor(named : "mainYellow")
+        label.textColor = Colors.mainYellow.color
         label.font = UIFont.systemFont(ofSize: 18)
         label.text = "----"
         return label
